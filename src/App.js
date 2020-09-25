d2f5f5828406d79725cc4e41f810c75adefe36eb
Quentin Vandevorst <56817496+Survive95@users.noreply.github.com>
2020-09-25 13:47:32 +0200

    installBtn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home';
@@ -18,6 +18,29 @@ function App() {
     firebase.signoutUser()
     localStorage.removeItem('token')
   })
+
+  const [defferedPrompt, setDefferedPrompt] = useState(null)
+  const [installBtn, setInstallBtn] = useState(false)
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    setDefferedPrompt(e)
+    setInstallBtn(true)
+  })
+
+  const install = () => {
+    setInstallBtn(false)
+    defferedPrompt.prompt()
+    defferedPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("installation réussie");
+      } else {
+        console.log("installation refusée");
+      }
+
+      setDefferedPrompt(null)
+    });
+  }
   
   return (
     <div className="App">
@@ -29,6 +52,7 @@ function App() {
         <Route exact path="/forgetpassword" component={ForgetPassword}></Route>
         <Route exact path="/edit/:id" component={Edit}></Route>
       </Router>
+      {installBtn ? <button onClick={install} className="installer"><i className="fas fa-download"></i></button> : ''}
     </div>
   );
 }
